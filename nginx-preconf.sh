8b3e3a31155844cce05d79c749be6dcb631e079a
Carmine Fabrizio <carmine.fabrizio@gig.com>
2019-02-25 21:26:08 +0000

    Test commit

--- nginx-preconf.sh
+++ nginx-preconf.sh
@@ -1,13 +1,53 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-git checkout -b $1
+#########################
+# The command line help #
+#########################
+display_help() {
+    echo "Usage: $0 [option...] {new_branch|add|commit}" >&2
+    echo
+    echo "   commit            You must put a comment or description"
+    echo
+    # echo some stuff here for the -a or --add-options
+    exit 1
+}
 
-git add *
+create_new_branch() {
+    echo "Function to create a new branch on github"
+    git checkout -b nginx-conf-branch
+    exit 0
+}
 
-git commit -m "$2"
+commit() {
+    echo "Function to add your changes to your branch"
+    git add * && git commit -a
+    exit 0
+}
 
-git push origin $1
+push() {
+    echo "Function to push your changes to origin and return to master branch"
+    git push origin nginx-conf-branch && git checkout master
+    exit 0
+}
 
-git branch -a
 
-git checkout master
+#create_new_branch
+
+case "$1" in
+    new_branch)
+        create_new_branch
+        exit 1
+        ;;
+    -h | --help)
+        display_help
+        exit 1
+        ;;
+    push)
+        push
+        exit 1
+        ;;
+    commit)
+        commit
+        exit 1
+        ;;
+esac
